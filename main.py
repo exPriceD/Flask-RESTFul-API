1cfb2fbaec569ae27c11f145a42c691abe774563
exPriceD <lolerpro4@gmail.com>
2023-09-20 01:52:34 +0300

    json -> sqlite, +models

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import request, Response
-from werkzeug.security import generate_password_hash, check_password_hash
 from validate_email import validate_email
 from config import application, db
+from models import Lessons, Personalities, Groups
 import json
 import os
 import typing as tp
@@ -12,50 +12,51 @@ PERSONALITIES_KEYS = ("id", "fio", "gender", "phone", "email", "work", "educatio
 @application.route('/api/v1/schedule', defaults={'group': None}, methods=["GET"])
 @application.route('/api/v1/schedule/<string:group>', methods=["GET"])
 def get_schedule_on_week(group: str):
-    response_data = {}
+    response_data = {"data": []}
     if not group:
-        groups = os.listdir(f"{os.getcwd()}\\groups")
+        groups = Groups.query.all()
+        print(groups)
         for group_name in groups:
-            even_schedule, odd_schedule = get_schedule_json(group_name)
-            response_data[group_name] = {"even_week": even_schedule, "odd_week": odd_schedule}
+            lessons_list = []
+            lessons = Lessons.query.filter_by(group=group_name.name).all()
+            for lesson in lessons:
+                lesson_data = {
+                    "id": lesson.id,
+                    "day": lesson.day,
+                    "even_week": lesson.even_week,
+                    "subject": lesson.subject,
+                    "type": lesson.type,
+                    "time_start": lesson.time_start,
+                    "time_end": lesson.time_end,
+                    "teacher_name": lesson.teacher_name,
+                    "room": lesson.room,
+                    "address": lesson.address,
+                    "zoom_url": lesson.zoom_url
+                }
+                lessons_list.append(lesson_data)
+            response_data["data"].append({group_name.name: lessons_list})
     else:
         try:
             even_schedule, odd_schedule = get_schedule_json(group)
         except OSError:
-            return Response(
-                response="Not Found",
-                status=404,
-            )
+            return Response(response="Not Found", status=404)
         response_data[group] = {"even_week": even_schedule, "odd_week": odd_schedule}
-    return Response(
-        response=json.dumps({response_data}, ensure_ascii=False),
-        status=200,
-        mimetype='application/json',
-    )
+    print(response_data)
+    return Response(response=json.dumps(response_data, ensure_ascii=False), status=200, mimetype='application/json',)
 
 
 @application.route('/api/v1/schedule/<string:group>/<string:week_number>', methods=["GET"])
 def get_schedule_of_week_number(group: str, week_number: str):
     even_schedule, odd_schedule = get_schedule_json(group)
     if week_number.isdigit():
-        if int(week_number) % 2 == 0:
-            response_data = {"even_week": even_schedule}
-        else:
-            response_data = {"odd_week": odd_schedule}
+        response_data = {"even_week": even_schedule} if int(week_number) % 2 == 0 else {"odd_week": odd_schedule}
     elif week_number == "even":
         response_data = {"even_week": even_schedule}
     elif week_number == "odd":
         response_data = {"odd_week": odd_schedule}
     else:
-        return Response(
-            response="Week not Found",
-            status=404,
-        )
-    return Response(
-        response=json.dumps({response_data}, ensure_ascii=False),
-        status=200,
-        mimetype='application/json',
-    )
+        return Response(response="Week not Found", status=404,)
+    return Response(response=json.dumps({response_data}, ensure_ascii=False), status=200, mimetype='application/json')
 
 
 @application.route('/api/v1/personalities', methods=["GET"])
@@ -66,7 +67,7 @@ def get_person():
 
 
 @application.route('/api/v1/personalities/<int:person_id>', methods=["GET"])
-def get_person_by_id(person_id):
+def get_person_by_id(person_id: int):
     with open(f"{os.getcwd()}\\personalities\\personalities.json", "r") as persons:
         personalities = json.load(persons)
     is_found = False
@@ -77,7 +78,8 @@ def get_person_by_id(person_id):
             is_found = True
             break
     if not is_found:
-        return Response(response={"status": "500", "reason": "Пользователь не найден"}, status=500, mimetype='application/json')
+        resp = {"status": "500", "reason": "Пользователь не найден"}
+        return Response(response=json.dumps(resp, ensure_ascii=False), status=500, mimetype='application/json')
     return Response(response=json.dumps(search_person, ensure_ascii=False), status=200, mimetype='application/json')
 
 @application.route('/api/v1/personalities', methods=["POST"])
@@ -94,14 +96,11 @@ def add_person():
 
 
 @application.route('/api/v1/personalities/<int:person_id>', methods=["PUT"])
-def edit_person(person_id):
+def edit_person(person_id: int):
     value = request.json
     if not all(key in value.keys() for key in PERSONALITIES_KEYS):
-        return Response(
-            response=json.dumps({"status": 500, "reason": "Поля заполнены некорректно"}),
-            status=500,
-            mimetype='application/json'
-        )
+        resp = {"status": 500, "reason": "Поля заполнены некорректно"}
+        return Response(response=json.dumps(resp, ensure_ascii=False), status=500, mimetype='application/json')
     with open(f"{os.getcwd()}\\personalities\\personalities.json", "r") as persons:
         personalities = json.load(persons)
     is_found = False
@@ -111,11 +110,8 @@ def edit_person(person_id):
             is_found = True
             break
     if not is_found:
-        return Response(
-            response=json.dumps({"status": "500", "reason": "Пользователь не найден"}),
-            status=500,
-            mimetype='application/json'
-        )
+        resp = {"status": "500", "reason": "Пользователь не найден"}
+        return Response(response=json.dumps(resp, ensure_ascii=False), status=500, mimetype='application/json')
     with open(f"{os.getcwd()}\\personalities\\personalities.json", "w") as persons:
         json.dump(personalities, persons, indent=4, ensure_ascii=False)
     return Response(response=json.dumps(value, ensure_ascii=False), status=200, mimetype='application/json')
@@ -132,3 +128,5 @@ def get_schedule_json(group_name: str) -> tp.Tuple:
 if __name__ == "__main__":
     application.debug = True
     application.run()
+    with application.app_context():
+        db.create_all()
---
+++ models.py
@@ -0,0 +1,46 @@
+from config import db
+
+
+class Groups(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(6))
+    faculty = db.Column(db.String(512))
+    direction = db.Column(db.String(512))
+    people_count = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Group {self.id} {self.name}>"
+
+
+class Personalities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fio = db.Column(db.String(256))
+    gender = db.Column(db.String(32))
+    phone = db.Column(db.String(32))
+    email = db.Column(db.String(256))
+    work = db.Column(db.String(512))
+    education = db.Column(db.String(512))
+
+    def __repr__(self):
+        return f"<Person {self.id}"
+
+
+class Lessons(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group = db.Column(db.String(6))
+    day = db.Column(db.String(16))
+    even_week = db.Column(db.Boolean)
+    subject = db.Column(db.String(512))
+    type = db.Column(db.String(512))
+    time_start = db.Column(db.String(5))
+    time_end = db.Column(db.String(5))
+    teacher_name = db.Column(db.String(512))
+    room = db.Column(db.String(512))
+    address = db.Column(db.String(512))
+    zoom_url = db.Column(db.String(1024))
+
+    def __repr__(self):
+        return f"Lessons {self.group}"
+
+
+
